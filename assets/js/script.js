b842516a2ba0809fcfd7f50cdc91207d75411956
prabhatsih <prabhats1410@gmail.com>
2023-12-01 19:55:41 +0530

    Slider Work Done

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+var sliderMain = document.querySelector('.slider-main');
+let item = sliderMain.getElementsByClassName('item');
+
+function next(){
+    sliderMain.append(item[0]);
+}
+function prev(){
+    sliderMain.prepend(item[item.length-1]);
+}
