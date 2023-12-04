1f11610f3bce148490c6c0fa1ee391139bec6d99
prabhatsih <prabhats1410@gmail.com>
2023-12-04 19:43:51 +0530

    ucomming event complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,10 @@ let item = sliderMain.getElementsByClassName('item');
 function next(){
     sliderMain.append(item[0]);
 }
+
 function prev(){
     sliderMain.prepend(item[item.length-1]);
 }
+
+setInterval(
+    next, 1000)
